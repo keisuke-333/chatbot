7d538d4d1507949221dfdcaa325f89bda58f928c
keisuke-333 <keikubo.999@gmail.com>
2021-02-05 00:08:30 +0900

    ライフサイクルの活用

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,13 @@ export default class App extends React.Component {
   selectAnswer = (selectAnswer, nextQuestionId) => {
     switch (true) {
       case (nextQuestionId === 'init'):
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
+        break;
+      case (/^https:*/.test(nextQuestionId)):
+        const a = document.createElement('a');
+        a.href = nextQuestionId;
+        a.target = '_blank';
+        a.click();
         break;
       default:
         const chats = this.state.chats;
@@ -45,7 +51,7 @@ export default class App extends React.Component {
           chats: chats
         })
 
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
         break;
     }
   }
@@ -53,6 +59,13 @@ export default class App extends React.Component {
   componentDidMount() {
     const initAnswer = "";
     this.selectAnswer(initAnswer, this.state.currentId)
+  }
+
+  componentDidUpdate() {
+    const scrollArea = document.getElementById('scroll-area');
+    if (scrollArea) {
+      scrollArea.scrollTop = scrollArea.scrollHeight;
+    }
   }
 
   render() {
